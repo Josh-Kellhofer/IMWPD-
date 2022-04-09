6feaf24f06b2f1219e74d4b6a2dcd54a198c2faa
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-04-09 09:36:31 -0700

    Added Links for NavBar.

--- frontend/src/components/Navbar2.js
+++ frontend/src/components/Navbar2.js
@@ -8,6 +8,7 @@ function Navbar2() {
   const [click, setClick] = useState(false) //sets the menu-icon to false (The icon itself)
 
   const handleClick = () => setClick(!click) //turns the menu-icon to an X
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
@@ -19,6 +20,28 @@ function Navbar2() {
         <div className ='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> 
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'} >
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+              Services
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+              Products
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
         </div>
         <div className='menu-icon' onClick={handleClick}>
           
